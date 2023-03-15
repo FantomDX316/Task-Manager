d7c7db779d7cc7fd606c44f3e3226563a73949b4
saksham <phantomdx316@gmail.com>
2023-03-15 21:30:21 +0530

    added logic for pagination

--- Backend/routes/taskRoute.js
+++ Backend/routes/taskRoute.js
@@ -48,9 +48,20 @@ router.post("/createtask",createTaskMiddleware,async (req, res)=>{
 
 //ROUTE 3 - List task ( http://localhost:5000/api/tasklist )
 
+// here we need to provide the TaskList id inorder to fetch all the tasks 
+
 router.get("/tasklist/:id",async (req,res)=>{
     try{
-    const Task = await TasksModel.find({taskListID:req.params.id},{taskListName:1,taskName:1,description:1,periodType:1,period:1,dueDate:1});
+
+    let {page,limit}  = req.query;
+    if(!page){
+        page = 1;
+    }else if(!limit){
+        limit = 5;
+    }else if(!page && !limit){
+        page = 1;
+    }   
+    const Task = await TasksModel.find({taskListID:req.params.id},{taskListName:1,taskName:1,description:1,periodType:1,period:1,dueDate:1}).skip((page-1)*limit).limit(limit);
     
     const taskItems = Task.map((item)=>{
          const dueDateIST = `${item.dueDate.getDate()}-${item.dueDate.getMonth()+1}-${item.dueDate.getFullYear()}`;
@@ -58,9 +69,10 @@ router.get("/tasklist/:id",async (req,res)=>{
         
         return {...item._doc,dueDate:dueDateIST};
          
-    })
+    });
+    const tasks = {taskCount:taskItems.length,tasks:taskItems};
 
-    res.status(200).json(taskItems);
+    res.status(200).json(tasks);
     
     }catch(error){
         res.status(500).json({"success":"false"})
