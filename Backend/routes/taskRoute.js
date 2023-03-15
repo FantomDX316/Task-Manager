9cb8c945ae8bb8ce55160d00615710bfe5e33696
saksham <phantomdx316@gmail.com>
2023-03-16 00:37:34 +0530

    added logic for search text

--- Backend/routes/taskRoute.js
+++ Backend/routes/taskRoute.js
@@ -8,20 +8,20 @@ const createTaskMiddleware = require("../middlewares/createTaskMiddleware")
 
 //ROUTE 1 - Create task list ( http://localhost:5000/api/createtasklist )
 
-router.post("/createtasklist",async (req,res)=>{
-    try{
+router.post("/createtasklist", async (req, res) => {
+    try {
 
-    const {name,description,active} = req.body;
-    const TaskList = new TaskListModel({
-        name,description,active
-    });
-    const listItem = await TaskList.save();
-    res.json({"success":true,"id":listItem.id});
-    }catch(error){
-        res.status(500).json({"error":"Internal Server Error"});
+        const { name, description, active } = req.body;
+        const TaskList = new TaskListModel({
+            name, description, active
+        });
+        const listItem = await TaskList.save();
+        res.json({ "success": true, "id": listItem.id });
+    } catch (error) {
+        res.status(500).json({ "error": "Internal Server Error" });
         console.log(error);
     };
-    
+
 
 });
 
@@ -32,17 +32,17 @@ router.post("/createtasklist",async (req,res)=>{
 // set taskListID as header key with value as the document id of taskList inorder to be able to create a task
 // Example = "taskListID":"sadh23hhu3uh25h23h"
 
-router.post("/createtask",createTaskMiddleware,async (req, res)=>{
-    const {taskName,description,period,periodType,taskListID} = req.body;
+router.post("/createtask", createTaskMiddleware, async (req, res) => {
+    const { taskName, description, period, periodType, taskListID } = req.body;
     const dueDate = req.dueDate;
     const TaskList = await TaskListModel.findById(taskListID);
     taskListName = TaskList.name;
-   
+
     const task = new TasksModel({
-        taskListName,taskName,description,dueDate,period,periodType,taskListID
+        taskListName, taskName, description, dueDate, period, periodType, taskListID
     });
-    task.save().then((task)=>{res.json(task)});
-    
+    task.save().then((task) => { res.json(task) });
+
 });
 
 
@@ -50,34 +50,40 @@ router.post("/createtask",createTaskMiddleware,async (req, res)=>{
 
 // here we need to provide the TaskList id inorder to fetch all the tasks 
 
-router.get("/tasklist/:id",async (req,res)=>{
-    try{
+router.get("/tasklist/:id", async (req, res) => {
+    try {
 
-    let {page,limit}  = req.query;
-    if(!page){
-        page = 1;
-    }else if(!limit){
-        limit = 5;
-    }else if(!page && !limit){
-        page = 1;
-    }   
-    const Task = await TasksModel.find({taskListID:req.params.id},{taskListName:1,taskName:1,description:1,periodType:1,period:1,dueDate:1}).skip((page-1)*limit).limit(limit);
-    
-    const taskItems = Task.map((item)=>{
-         const dueDateIST = `${item.dueDate.getDate()}-${item.dueDate.getMonth()+1}-${item.dueDate.getFullYear()}`;
-         
+        let { page, limit, searchText } = req.query;
+        if (!page || !limit) {
+            page = 1;
+            limit = 5;
+        };
         
-        return {...item._doc,dueDate:dueDateIST};
-         
-    });
-    const tasks = {taskCount:taskItems.length,tasks:taskItems};
 
-    res.status(200).json(tasks);
-    
-    }catch(error){
-        res.status(500).json({"success":"false"})
+        const Task = await TasksModel.find({ taskListID: req.params.id,
+        $or:[
+            {taskName:{$regex:'.*'+searchText+'.*',$options:'i'}},
+            {description:{$regex:'.*'+searchText+'.*',$options:'i'}}
+        ] }, { taskListName: 1, taskName: 1, description: 1, periodType: 1, period: 1, dueDate: 1 }).skip((page - 1) * limit).limit(limit);
+        
+        let taskItems = Task.map((item) => {
+            const dueDateIST = `${item.dueDate.getDate()}-${item.dueDate.getMonth() + 1}-${item.dueDate.getFullYear()}`;
+            const periodIST = `${item.period.getDate()}-${item.period.getMonth() + 1}-${item.period.getFullYear()}`;
+
+
+            return { ...item._doc, dueDate: dueDateIST,period : periodIST };
+
+        });
+        
+        const tasks = { taskCount: taskItems.length, tasks:taskItems };
+        console.log(taskItems);
+
+        res.status(200).json(tasks);
+
+    } catch (error) {
+        res.status(500).json({ "success": "false" })
     }
-    
+
 })
 
 module.exports = router;
